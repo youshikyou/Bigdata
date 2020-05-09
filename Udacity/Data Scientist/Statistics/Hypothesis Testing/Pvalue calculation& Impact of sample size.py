c8d696e7ed83f4251338471df216a2d90f1b98ce
youshikyou <35782965+youshikyou@users.noreply.github.com>
2020-05-09 20:11:42 +0200

    Update Pvalue calculation& Impact of sample size.py

--- Udacity/Data Scientist/Statistics/Hypothesis Testing/Pvalue calculation& Impact of sample size.py
+++ Udacity/Data Scientist/Statistics/Hypothesis Testing/Pvalue calculation& Impact of sample size.py
@@ -9,8 +9,6 @@
 # 
 # Start by reading in the libraries and data.
 
-# In[2]:
-
 
 import pandas as pd
 import numpy as np
@@ -31,26 +29,13 @@ full_data = pd.read_csv('coffee_dataset.csv')
 
 # `2.` What is the population mean?  Create a sample set of data using the below code.  What is the sample mean?  What is the standard deviation of the population?  What is the standard deviation of the sampling distribution of the mean of five draws? Simulate the sampling distribution for the mean of five values to see the shape and plot a histogram. Use **quiz 2** below to assure your answers are correct.  
 
-# In[3]:
-
 
 sample1 = full_data.sample(5)
 sample1
 
-
-# In[4]:
-
-
 full_data.height.mean() # Population mean
 
-
-# In[5]:
-
-
 sample1.height.mean() # Sample mean
-
-
-# In[6]:
 
 
 sampling_dist_mean5 = []
@@ -62,10 +47,6 @@ for _ in range(10000):
     
 plt.hist(sampling_dist_mean5);
 
-
-# In[7]:
-
-
 std_sampling_dist = np.std(sampling_dist_mean5)
 std_sampling_dist# the standard deviation of the sampling distribution
 
@@ -74,17 +55,11 @@ std_sampling_dist# the standard deviation of the sampling distribution
 # 
 # **Hint:** Use the numpy documentation [here](https://docs.scipy.org/doc/numpy-1.13.0/reference/generated/numpy.random.normal.html) to assist with your solution.
 
-# In[8]:
-
-
 null_mean = 67.60
 null_vals = np.random.normal(null_mean, std_sampling_dist, 10000)
 
 plt.hist(null_vals);
 plt.axvline(x=sample1.height.mean(), color = 'red'); # where our sample mean falls on null dist
-
-
-# In[9]:
 
 
 # for a two sided hypothesis, we want to look at anything 
@@ -101,9 +76,9 @@ pval = prob_more_extreme_low + prob_more_extreme_high
 pval
 
 
-# **The above shows a second possible method for obtaining the p-value.  These are pretty different, stability of these values with such a small sample size is an issue. We are essentially shading outside the lines below.**
-
-# In[10]:
+# **The above shows a second possible method for obtaining the p-value.  
+#These are pretty different, stability of these values with such a small sample size is an issue.
+#We are essentially shading outside the lines below.**
 
 
 upper_bound = obs_mean
@@ -114,9 +89,6 @@ plt.axvline(x=lower_bound, color = 'red'); # where our sample mean falls on null
 plt.axvline(x=upper_bound, color = 'red'); # where our sample mean falls on null dist
 
 
-# In[11]:
-
-
 print(upper_bound, lower_bound)
 
 
@@ -124,16 +96,9 @@ print(upper_bound, lower_bound)
 # 
 # **Hint:**  If you get stuck, notice you can use the solution from quiz regarding finding the p-value earlier to assist with obtaining this answer with just a few small changes.
 
-# In[12]:
-
-
 sample2 = full_data.sample(300)
 
 obs_mean = sample2.height.mean()
-
-
-# In[13]:
-
 
 sampling_dist_mean300 = []
 
@@ -146,18 +111,12 @@ std_sampling_dist300 = np.std(sampling_dist_mean300)
 null_vals = np.random.normal(null_mean, std_sampling_dist300, 10000)
 
 
-# In[14]:
-
-
 upper_bound = obs_mean
 lower_bound = null_mean - (obs_mean - null_mean)
 
 plt.hist(null_vals);
 plt.axvline(x=lower_bound, color = 'red'); # where our sample mean falls on null dist
 plt.axvline(x=upper_bound, color = 'red'); # where our sample mean falls on null dist
-
-
-# In[16]:
 
 
 # for a two sided hypothesis, we want to look at anything 
@@ -177,9 +136,6 @@ pval  # With such a large sample size, our sample mean that is super
 # `5.` Reflect on what happened by answering the final quiz in this concept.
 
 # **Even with a very small difference between a sample mean and a hypothesized population mean, the difference will end up being significant with a very large sample size.**
-
-# In[ ]:
-
 
 
 
