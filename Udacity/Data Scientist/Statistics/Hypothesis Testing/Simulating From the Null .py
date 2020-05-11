2aa304661192ec4fa827e6fe82fe4bc01862a781
youshikyou <35782965+youshikyou@users.noreply.github.com>
2020-05-11 17:30:38 +0200

    Update Simulating From the Null .py

--- Udacity/Data Scientist/Statistics/Hypothesis Testing/Simulating From the Null .py
+++ Udacity/Data Scientist/Statistics/Hypothesis Testing/Simulating From the Null .py
@@ -17,23 +17,27 @@ full_data = pd.read_csv('coffee_dataset.csv')
 sample_data = full_data.sample(200)
 
 
-# `1.` If you were interested in studying whether the average height for coffee drinkers is the same as for non-coffee drinkers, what would the null and alternative hypotheses be?  Write them in the cell below, and use your answer to answer the first quiz question below.
+# 1. If you were interested in studying whether the average height for coffee drinkers is the same as for non-coffee drinkers, 
+# what would the null and alternative hypotheses be? 
 
 # **Since there is no directional component associated with this statement, a not equal to seems most reasonable.**
 # 
 # $$H_0: \mu_{coff} - \mu_{no} = 0$$
 # 
 # 
-# $$H_1: \mu_{coff} - \mu_{no} \neq 0$$
+# $$H_1: \mu_{coff} - \mu_{no} != 0$$
 # 
 # 
 # **$\mu_{coff}$ and $\mu_{no}$ are the population mean values for coffee drinkers and non-coffee drinkers, respectivley.**
 
-# `2.` If you were interested in studying whether the average height for coffee drinkers is less than non-coffee drinkers, what would the null and alternative be?  Place them in the cell below, and use your answer to answer the second quiz question below.
+# 2.If you were interested in studying whether the average height for coffee drinkers is less than non-coffee drinkers,
+# what would the null and alternative be? 
 
-# **In this case, there is a question associated with a direction - that is the average height for coffee drinkers is less than non-coffee drinkers.  Below is one of the ways you could write the null and alternative.  Since the mean for coffee drinkers is listed first here, the alternative would suggest that this is negative.**
+# **In this case, there is a question associated with a direction -
+# that is the average height for coffee drinkers is less than non-coffee drinkers.rnative.
+# Since the mean for coffee drinkers is listed first here, the alternative would suggest that this is negative.**
 # 
-# $$H_0: \mu_{coff} - \mu_{no} \geq 0$$
+# $$H_0: \mu_{coff} - \mu_{no} >= 0$$
 # 
 # 
 # $$H_1: \mu_{coff} - \mu_{no} < 0$$
@@ -41,9 +45,9 @@ sample_data = full_data.sample(200)
 # 
 # **$\mu_{coff}$ and $\mu_{no}$ are the population mean values for coffee drinkers and non-coffee drinkers, respectivley.**
 
-# `3.` For 10,000 iterations: bootstrap the sample data, calculate the mean height for coffee drinkers and non-coffee drinkers, and calculate the difference in means for each sample.  You will want to have three arrays at the end of the iterations - one for each mean and one for the difference in means.  Use the results of your sampling distribution, to answer the third quiz question below.
-
-
+# 3.For 10,000 iterations: bootstrap the sample data, calculate the mean height for coffee drinkers and non-coffee drinkers, 
+# and calculate the difference in means for each sample.
+# You will want to have three arrays at the end of the iterations - one for each mean and one for the difference in means.  
 
 nocoff_means, coff_means, diffs = [], [], []
 
@@ -56,8 +60,6 @@ for _ in range(10000):
     nocoff_means.append(nocoff_mean)
     diffs.append(coff_mean - nocoff_mean)   
     
-
-
 
 np.std(nocoff_means) # the standard deviation of the sampling distribution for nocoff
 
@@ -78,9 +80,15 @@ plt.hist(coff_means, alpha = 0.5); # They look pretty normal to me!
 plt.hist(diffs, alpha = 0.5); # again normal - this is by the central limit theorem
 
 
-# `4.` Now, use your sampling distribution for the difference in means and [the docs](https://docs.scipy.org/doc/numpy-1.13.0/reference/generated/numpy.random.normal.html) to simulate what you would expect if your sampling distribution were centered on zero.  Also, calculate the observed sample mean difference in `sample_data`. Use your solutions to answer the last questions in the quiz below.
+# 4. Now, use your sampling distribution for the difference in means and 
+#(https://docs.scipy.org/doc/numpy-1.13.0/reference/generated/numpy.random.normal.html) to simulate what you would expect
+# if your sampling distribution were centered on zero.  
+# Also, calculate the observed sample mean difference in `sample_data`. 
 
-# ** We would expect the sampling distribution to be normal by the Central Limit Theorem, and we know the standard deviation of the sampling distribution of the difference in means from the previous question, so we can use this to simulate draws from the sampling distribution under the null hypothesis.  If there is truly no difference, then the difference between the means should be zero.**
+# ** We would expect the sampling distribution to be normal by the Central Limit Theorem, 
+# and we know the standard deviation of the sampling distribution of the difference in means from the previous question, 
+# so we can use this to simulate draws from the sampling distribution under the null hypothesis. 
+# If there is truly no difference, then the difference between the means should be zero.**
 
 
 null_vals = np.random.normal(0, np.std(diffs), 10000) # Here are 10000 draws from the sampling distribution under the null
@@ -88,6 +96,4 @@ null_vals = np.random.normal(0, np.std(diffs), 10000) # Here are 10000 draws fro
 
 
 plt.hist(null_vals); #Here is the sampling distribution of the difference under the null
-
-
 
