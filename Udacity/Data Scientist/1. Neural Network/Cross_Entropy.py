7b88d10682fbe637cc6c9f2954f31ef61b4db374
youshikyou <35782965+youshikyou@users.noreply.github.com>
2020-05-20 14:02:25 +0200

    Update Cross_Entropy.py

--- Udacity/Data Scientist/1. Neural Network/Cross_Entropy.py
+++ Udacity/Data Scientist/1. Neural Network/Cross_Entropy.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-# Write a function that takes as input two lists Y, P,
+# Write a function that takes as input two lists Y, P, Y is the label and P is the predicted pobability corresponding the label
 # and returns the float corresponding to their cross-entropy.
 def cross_entropy(Y, P):
         #sum{y_i * ln(p_i) + (1-y_i)*ln(1-pi)} # 1~n
