716ae943a17e0c8f4e9cdcb74a5d09eb02b664c8
youshikyou <35782965+youshikyou@users.noreply.github.com>
2020-06-04 11:30:58 +0200

    Update acquire information.py

--- Pandas/acquire information.py
+++ Pandas/acquire information.py
@@ -1,7 +1,12 @@
+#read csv
 X = pd.read_csv('../input/train.csv', index_col='Id') 
 
+#show the first five rows
 X.head()
 X.tail()
 
+#each column statistic information
 X.describe()
 
+#column list
+X.columns
