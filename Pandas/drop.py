4cf6ad04ea38e8948afb5b0f0e4b63017576c28f
youshikyou <35782965+youshikyou@users.noreply.github.com>
2020-06-04 11:27:25 +0200

    Update drop.py

--- Pandas/drop.py
+++ Pandas/drop.py
@@ -2,4 +2,7 @@
 drop_X_train = X_train.select_dtypes(exclude=['object'])
 
 #drop columns with column name
-X_train.drop(['col1','col2'], axis=1)
+X_train.drop(['col1','col2'], axis=1,inplace=True)
+
+#drop rows with null in certain column
+X.dropna(axis=0, subset=['col'], inplace=True)
