2ec78d04cb0ea29bfcd7f22cbbaf540378ba9921
youshikyou <35782965+youshikyou@users.noreply.github.com>
2020-06-04 14:51:08 +0200

    Update Preprocess.py

--- Sklearn/Preprocess.py
+++ Sklearn/Preprocess.py
@@ -32,4 +32,56 @@ count_enc = ce.CountEncoder()
 count_encoded = count_enc.fit_transform(df[features])
 data = baseline_data.join(count_encoded.add_suffix("_count"))
 
+====================================================================================
+#Target encoding replaces a categorical value with the average value of the target for that value of the feature. 
+#For example, given the country value "CA", you'd calculate the average outcome for all the rows with country == 'CA', around 0.28. 
+#This is often blended with the target probability over the entire dataset to reduce the variance of values with few occurences.
+
+#This technique uses the targets to create new features. 
+#So including the validation or test data in the target encodings would be a form of target leakage. 
+#Instead, you should learn the target encodings from the training dataset only and apply it to the other datasets.
+
+# Create the encoder itself
+
+import category_encoders as ce
+target_enc = ce.TargetEncoder(cols=features)
+# Fit the encoder using the categorical features and target
+target_enc.fit(train[features], train['target'])
+train = train.join(target_enc.transform(train[features]).add_suffix('_target'))            
+
+====================================================================================
+#CatBoost Encoding
+#Finally, we'll look at CatBoost encoding. 
+#This is similar to target encoding in that it's based on the target probablity for a given value. 
+#However with CatBoost, for each row, the target probability is calculated only from the rows before it. 
+
+target_enc = ce.CatBoostEncoder(cols=features)
+target_enc.fit(train[features], train['target'])
+train = train.join(target_enc.transform(train[features]).add_suffix('_cb'))                                                            
+                                                            
+                                                            
+                                                            
+                                                            
+                                                            
+                                                            
+                                                            
+                                                            
+                                                            
+                                                            
+                                                            
+                                                            
+                                                            
+                                                            
+                                                            
+                                                            
+                                                            
+                                                            
+                                                            
+                                                            
+                                                            
+                                                            
+                                                            
+                                                            
+                                                            
+                                                            
                                                             
