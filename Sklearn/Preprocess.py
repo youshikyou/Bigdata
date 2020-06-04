867d0eea1de12e95439292a91b7c982400904478
youshikyou <35782965+youshikyou@users.noreply.github.com>
2020-06-04 12:15:57 +0200

    Update Preprocess.py

--- Sklearn/Preprocess.py
+++ Sklearn/Preprocess.py
@@ -9,9 +9,9 @@ df = df[[otherfeatures]].join(encoded)
 label_encoder = LabelEncoder()
 for feature in features:
   clicks[feature + '_labels'] = label_encoder.fit_transform(clicks[feature]
-                                                            
+
 ======================================================================================                                                     
-                                                            
+ 
 from sklearn.preprocessing import OneHotEncoder
 
 myOHEncoder = OneHotEncoder(sparse=False,handle_unknown='ignore') # sparse=True will be a matrix.
@@ -20,3 +20,11 @@ OH_cols_valid = pd.DataFrame(myOHEncoder.transform(X_valid['feature_list']))
 # One-hot encoding removed index; put it back
 OH_cols_train.index = X_train.index
 OH_cols_valid.index = X_valid.index
+
+=======================================================================================
+#Count Encoding
+#Count encoding replaces each categorical value with the number of times it appears in the dataset. 
+#For example, if the value "GB" occured 10 times in the country feature, then each "GB" would be replaced with the number 10.
+#We'll use the categorical-encodings package to get this encoding. The encoder itself is available as CountEncoder. 
+#This encoder and the others in categorical-encodings work like scikit-learn transformers with .fit and .transform methods
+import category_encoders as ce
