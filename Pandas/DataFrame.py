34da58b2942fd0be26b6b1dce4e144da0ab07289
youshikyou <35782965+youshikyou@users.noreply.github.com>
2020-06-04 17:27:51 +0200

    Create DataFrame.py

---
+++ Pandas/DataFrame.py
@@ -0,0 +1,9 @@
+df.diff()  
+First discrete difference of element.
+Calculates the difference of a DataFrame element compared with another element in the DataFrame 
+(default is the element in the same column of the previous row).
+
+df.rolling()
+Provide rolling window calculations like df.rolling('6H'), df.rolling('7d')， past 6 hours, past 7 days.
+
+df.expanding()
