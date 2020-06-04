636a5769b50a27722bfef5d7643d9656c31933e0
youshikyou <35782965+youshikyou@users.noreply.github.com>
2020-06-04 11:24:16 +0200

    Update Preprocess.py

--- Sklearn/Preprocess.py
+++ Sklearn/Preprocess.py
@@ -9,3 +9,15 @@ df = df[[otherfeatures]].join(encoded)
 label_encoder = LabelEncoder()
 for feature in features:
   clicks[feature + '_labels'] = label_encoder.fit_transform(clicks[feature])
+
+  
+  
+  
+from sklearn.preprocessing import OneHotEncoder
+
+myOHEncoder = OneHotEncoder(sparse=False,handle_unknown='ignore') # sparse=True will be a matrix.
+OH_cols_train = pd.DataFrame(myOHEncoder.fit_transform(X_train['feature list']))
+OH_cols_valid = pd.DataFrame(myOHEncoder.transform(X_valid['feature_list']))
+# need index again
+OH_cols_train.index = X_train.index
+OH_cols_valid.index = X_valid.index
