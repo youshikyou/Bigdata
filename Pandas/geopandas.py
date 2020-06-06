23d0664ffeb8a720b78f2d51623f1cbc94acd85c
youshikyou <35782965+youshikyou@users.noreply.github.com>
2020-06-06 11:28:43 +0200

    Update geopandas.py

--- Pandas/geopandas.py
+++ Pandas/geopandas.py
@@ -1,2 +1,6 @@
 import geopandas as gpd
 gpd.read_file(filepath)
+
+#create map and plot another map in the same figure
+ax = world.plot(figsize=(20,20), color='whitesmoke', linestyle=':', edgecolor='black')
+df.plot(ax=ax, markersize=2)
