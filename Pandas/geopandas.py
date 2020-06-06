b1134ae3e862dab4958fa02502183658f048cc9e
youshikyou <35782965+youshikyou@users.noreply.github.com>
2020-06-06 13:07:57 +0200

    Update geopandas.py

--- Pandas/geopandas.py
+++ Pandas/geopandas.py
@@ -4,3 +4,10 @@ gpd.read_file(filepath)
 #create map and plot another map in the same figure
 ax = world.plot(figsize=(20,20), color='whitesmoke', linestyle=':', edgecolor='black')
 df.plot(ax=ax, markersize=2)
+
+"""
+To create a GeoDataFrame from a CSV file, we needed to use both Pandas and GeoPandas:
+We begin by creating a DataFrame containing columns with latitude and longitude coordinates.
+To convert it to a GeoDataFrame, we use gpd.GeoDataFrame().
+The gpd.points_from_xy() function creates Point objects from the latitude and longitude columns.
+"""
