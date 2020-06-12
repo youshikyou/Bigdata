010df7777c2de5d58982ee35ae1b436c06b71449
youshikyou <35782965+youshikyou@users.noreply.github.com>
2020-06-12 11:16:51 +0200

    Update colum.py

--- Pandas/colum.py
+++ Pandas/colum.py
@@ -5,3 +5,8 @@ df = df.assign(new_column=(df[feature] == 'condition').astype(int))
 df.loc[:, ["col1", "col2", "col3"]]
 
 df1 = df.query('col1 == "category1"').copy()
+
+
+
+pandas.DataFrame.select_dtypes
+DataFrame.select_dtypes(self, include=None, exclude=None) #Return a subset of the DataFrame’s columns based on the column dtypes.
