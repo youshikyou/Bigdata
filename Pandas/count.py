7eb5d38188eb59b378453c03ae6df2d48ef2ae60
youshikyou <35782965+youshikyou@users.noreply.github.com>
2020-06-16 11:51:42 +0200

    Update count.py

--- Pandas/count.py
+++ Pandas/count.py
@@ -3,3 +3,7 @@ X_train[col].nunique()
 
 # count the value in a column
 df.col1.value_counts()
+
+
+# count() count the value without nan and return the dataframe
+# size() count nan value and return the series
